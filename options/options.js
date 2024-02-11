6e4061cb29776a7a4631bca33b993a8d9c3da853
Crowdbotics <team@crowdbotics.com>
2024-02-11 10:00:25 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wild-violet-46246",
+  url: "https://wild-violet-46246.botics.co",
+  api: "https://wild-violet-46246.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
